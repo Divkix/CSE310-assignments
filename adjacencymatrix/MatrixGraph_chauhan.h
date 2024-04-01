fe87ab900acab3735d18811eff5cb27a6d8cf405
Divanshu Chauhan <divkix@divkix.me>
2024-04-01 10:39:29 -0700

    Add comments

--- adjacencymatrix/MatrixGraph_chauhan.h
+++ adjacencymatrix/MatrixGraph_chauhan.h
@@ -1,34 +1,37 @@
 #ifndef MATRIXGRAPH_CHAUHAN_H
 #define MATRIXGRAPH_CHAUHAN_H
 
+// import necessary libraries
 #include <string>
 #include <vector>
 
+// define the class MatrixGraph
 class MatrixGraph
 {
+    // define the private members of the class
 private:
-    int numVertices;
-    bool isDirected;
-    float **adjacencyMatrix;
+    int numVertices;         // number of vertices in the graph
+    bool isDirected;         // boolean to check if the graph is directed or not
+    float **adjacencyMatrix; // 2D array to store the adjacency matrix
+    void initializeMatrix(); // initialize the adjacency matrix
+    void deleteMatrix();     // delete the adjacency matrix
 
+    // define the public members of the class
 public:
-    MatrixGraph(int numVertices, bool isDirected);
-    ~MatrixGraph();
+    MatrixGraph(int numVertices, bool isDirected); // constructor
+    ~MatrixGraph();                                // destructor
 
-    void addEdge(int v1, int v2);
-    void addEdge(int v1, int v2, float weight);
-    void removeEdge(int v1, int v2);
-    bool adjacent(int v1, int v2);
-    float getEdgeWeight(int v1, int v2);
-    void setEdgeWeight(int v1, int v2, float weight);
-    std::string toString();
-    void printRaw();
-    bool pathExists(int v1, int v2);
-    std::vector<int> getBFSPath(int v1, int v2);
+    void addEdge(int v1, int v2);                     // add an edge between two vertices
+    void addEdge(int v1, int v2, float weight);       // add an edge between two vertices with a weight
+    void removeEdge(int v1, int v2);                  // remove an edge between two vertices
+    bool adjacent(int v1, int v2);                    // check if two vertices are adjacent
+    float getEdgeWeight(int v1, int v2);              // get the weight of the edge between two vertices
+    void setEdgeWeight(int v1, int v2, float weight); // set the weight of the edge between two vertices to a new value
+    void printRaw();                                  // print the raw adjacency matrix
+    bool pathExists(int v1, int v2);                  // check if a path exists between two vertices
 
-private:
-    void initializeMatrix();
-    void deleteMatrix();
+    std::string toString();                      // return a string representation of the graph
+    std::vector<int> getBFSPath(int v1, int v2); // get the path between two vertices using BFS
 };
 
 #endif // MATRIXGRAPH_CHAUHAN_H
--- adjacencymatrix/Queue_chauhan.hpp
+++ adjacencymatrix/Queue_chauhan.hpp
@@ -1,84 +1,112 @@
 #ifndef QUEUE_CHAUHAN_HPP
 #define QUEUE_CHAUHAN_HPP
 
+// import stdexcept library to use runtime_error
 #include <stdexcept>
 
+// define the class Queue
 template <typename T>
 class Queue
 {
+    // define the private members of the class
 private:
+    // struct Node to store the data and the next pointer
     struct Node
     {
-        T data;
-        Node *next;
-        Node(const T &d) : data(d), next(nullptr) {}
+        T data;                                      // data of the node
+        Node *next;                                  // pointer to the next node
+        Node(const T &d) : data(d), next(nullptr) {} // constructor
     };
 
-    Node *front;
-    Node *rear;
-    int size;
+    Node *front; // pointer to the front of the queue
+    Node *rear;  // pointer to the rear of the queue
+    int size;    // size of the queue
 
+    // define the public members of the class
 public:
-    Queue() : front(nullptr), rear(nullptr), size(0) {}
+    Queue() : front(nullptr), rear(nullptr), size(0) {} // constructor
 
+    // destructor
     ~Queue()
     {
+        // dequeue all the elements in the queue while it is not empty
         while (!isEmpty())
         {
-            dequeue();
+            dequeue(); // call the dequeue function
         }
     }
 
+    // enqueue function to add an element to the queue
     void enqueue(const T &item)
     {
+        // create a new node with the data
         Node *newNode = new Node(item);
+
+        // check if the queue is empty
         if (isEmpty())
         {
-            front = rear = newNode;
+            front = rear = newNode; // set the front and rear to the new node
         }
         else
+        // if the queue is not empty
         {
-            rear->next = newNode;
-            rear = newNode;
+            rear->next = newNode; // set the next pointer of the rear to the new node
+            rear = newNode;       // set the rear to the new node
         }
+
+        // increment the size of the queue
         size++;
     }
 
+    // dequeue function to remove an element from the queue
     T dequeue()
     {
+        // check if the queue is empty
         if (isEmpty())
         {
+            // throw a runtime error
             throw std::runtime_error("Queue is empty. Cannot dequeue.");
         }
-        Node *temp = front;
-        T data = temp->data;
-        front = front->next;
-        delete temp;
-        size--;
+
+        Node *temp = front;  // create a temporary node and set it to the front
+        T data = temp->data; // get the data of the front node
+        front = front->next; // set the front to the next node
+        delete temp;         // delete the temporary node
+        size--;              // decrement the size of the queue
+
+        // check if the queue is empty
         if (isEmpty())
         {
-            rear = nullptr;
+            rear = nullptr; // set the rear to null
         }
+
+        // return the data of the front node
         return data;
     }
 
+    // peek function to get the data of the front node
     T &peek() const
     {
+        // check if the queue is empty, if so, throw a runtime error
         if (isEmpty())
         {
-            throw std::runtime_error("Queue is empty. Cannot peek.");
+            throw std::runtime_error("Queue is empty. Cannot peek."); // we throw the runtime error here
         }
+
+        // return the data of the front node
         return front->data;
     }
 
+    // isEmpty function to check if the queue is empty
     bool isEmpty() const
     {
-        return size == 0;
+        return size == 0; // return true if the size is 0, false otherwise
     }
 
+    // getSize function to get the size of the queue
     int getSize() const
     {
-        return size;
+        return size; // return the size of the queue
     }
 };
 
